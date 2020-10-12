3e4aa83c8ce8134483324f81321a028655a75a59
valentina891 <valemil.vs@gmail.com>
2020-10-12 16:54:29 +0200

    Navbar e collegamenti

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <title>@yield('titolo')</title>
+    </head>
+    <body>
+
+        <header>
+            {{-- @include('partials/header') --}}
+        </header>
+
+        <main>
+            @yield('content')
+        </main>
+
+        <footer>
+            @include('partials/header')
+        </footer>
+
+
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', 'StaticPageController@home'->name('home');
+Route::get('/chisiamo', 'StaticPageController@chisiamo'->name('chisiamo');
 });
