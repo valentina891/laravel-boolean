fe15fe85ff4e824af6084490cd17da63f2f7e498
valentina891 <valemil.vs@gmail.com>
2020-10-13 23:17:23 +0200

    aggiunta genere

--- config/students.php
+++ config/students.php
@@ -5,6 +5,7 @@ return [
         'img'=> 'https://www.boolean.careers/images/people/ortenzi.png',
         'nome'=> 'Daniela Ortenzi',
         'anni'=> 27,
+        'genere'=> 'f',
         'azienda'=> 'RDS Radio Dimensione Suono',
         'ruolo'=> 'Web Developer',
         'descrizione' => 'Laureata in psicologia alla Sapienza di Roma con una nascente carriere nella SEO, Daniela si iscrive al corso Boolean. Ora lavora come web developer presso RDS Radio Dimensione Suono.'
@@ -13,6 +14,7 @@ return [
         'img'=> 'https://www.boolean.careers/images/people/antidormi.png',
         'nome'=> 'Nicholas Antidormi',
         'anni'=> 20,
+        'genere'=> 'm',
         'azienda'=> 'Atoms Studio',
         'ruolo'=> 'Web Developer',
         'descrizione' => 'Intraprende gli studi universitari in matematica per le sue doti analitiche, ma abbandona il percorso per concentrarsi su uno più pratico e orientato alla richiesta del mercato del lavoro: la programmazione.'
@@ -21,6 +23,7 @@ return [
         'img'=> 'https://www.boolean.careers/images/people/giannone.jpeg',
         'nome'=> 'Valentina Giannone',
         'anni'=> 38,
+        'genere'=> 'f',
         'azienda'=> 'Bloovery',
         'ruolo'=> 'Web Developer',
         'descrizione' => 'Un passato come responsabile di laboratorio odontotecnico, per poi decidere che non era la sua vera vocazione. Valentina ha deciso di prendere il coraggio a due mani e lanciarsi con successo nel mondo del web development.'
--- resources/views/studenti.blade.php
+++ resources/views/studenti.blade.php
@@ -11,6 +11,7 @@
                 <img src="{{ $student['img'] }}" class="card-img-top" alt="{{ $student['nome'] }}">
                 <div class="card-body">
                     <h5 class="card-title">{{ $student['nome'] }}</h5>
+                    <h6>Assunt{{ ($student['genere'] == 'f') ? 'a': 'o' }} da {{ $student['azienda'] }}</h6>
                     <p class="card-text">{{ $student['descrizione'] }}</p>
                     <p class="card-text"><small class="text-muted">{{ $student['anni'] }}</small></p>
                 </div>
